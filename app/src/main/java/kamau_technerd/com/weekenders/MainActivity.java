3c20b2f047d73bd7557a1eaa5829f4e42a595eec
kamau-mbugua <kamau_mbugua@yahoo.com>
2020-05-01 22:48:25 +0300

    first commit

---
+++ app/src/main/java/kamau_technerd/com/weekenders/MainActivity.java
@@ -0,0 +1,14 @@
+package kamau_technerd.com.weekenders;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
